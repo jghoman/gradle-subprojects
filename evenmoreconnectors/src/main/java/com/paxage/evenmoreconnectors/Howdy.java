b6fe3cf954a67697cafa8f2ad4965990b97c7bd1
Jakob Homan <jghoman@gmail.com>
2012-06-26 16:01:31 -0700

    Add variable interpolation and all-project flag.

---
+++ evenmoreconnectors/src/main/java/com/paxage/evenmoreconnectors/Howdy.java
@@ -0,0 +1,9 @@
+package com.paxage.evenmoreconnectors;
+
+import com.paxage.someconnectors.ConnectFooAndHadoop;
+
+public class Howdy {
+    public static ConnectFooAndHadoop getFAH() {
+        return new ConnectFooAndHadoop();
+    }
+}
